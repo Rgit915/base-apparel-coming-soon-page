95b5866fef7ef247257610eccf2fa4d52d57246c
Rgit915 <rora.goitom@gmail.com>
2024-05-20 11:07:10 -0400

    Add custom linear gradients to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ export default {
         desaturatedRed: "hsl(0, 36%, 70%)",
         softRed: "hsl(0, 93%, 68%)",
         darkGrayishRed: "hsl(0, 6%, 24%)",
+        linearGradient1: "hsl(0, 80%, 86%)",
+        linearGradient2: "hsl(0, 74%, 74%)",
       },
     },
 
